89be8d15b04e35b3762f6738bdffc99c9eeb389a
Ivan <lorf1991@gmail.com>
2022-03-26 14:13:02 +0200

    fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const http = require("http");
 const server = http.createServer(function (req, res) {
   console.log(req.url);
 
-  res.write("<h1>Hellow !!!!<h1>");
-  res.write("<h2>Hellow !!!!<h2>");
-  res.write("<h3>Hellow !!!!<h3>");
+  res.write("<h1>Hello !!!!<h1>");
+  res.write("<h2>Hello !!!!<h2>");
+  res.write("<h3>Hello !!!!<h3>");
   res.end();
 });
 const PORT = 3000;
