3b32d02ab86f6ceeee67fb59b5e9f22c484d1eaa
Ivan <lorf1991@gmail.com>
2022-03-26 14:11:44 +0200

    base work with http module

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const user = require("./src/user");
+const http = require("http");
 
-console.log("dirname ===> ", __dirname);
-console.log("filename ===> ", __filename);
-console.log("USER ===>", user);
-user.sayHEllo();
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+
+  res.write("<h1>Hellow !!!!<h1>");
+  res.write("<h2>Hellow !!!!<h2>");
+  res.write("<h3>Hellow !!!!<h3>");
+  res.end();
+});
+const PORT = 3000;
+
+server.listen(PORT, () =>
+  console.log(`server is running on port ${PORT} ! ...`)
+);
